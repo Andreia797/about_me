081459ba29c8e0ac07bf76b66abbe38417262915
Andreia797 <andreiasemedo81@gmail.com>
2026-02-03 12:32:33 -0100

    Update Imagens

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -16,21 +16,23 @@ export const projects: Project[] = [
     tech: ["Django", "React", "PostgreSQL"],
     type: "projectItems.1.type",
     github: "https://github.com/Andreia797",
-    image: "/TelaLogin.png"
+    image: "/TelaLoginUnicv.png"
   },
   {
     id: "2",
     title: "projectItems.2.title",
     description: "projectItems.2.description",
     tech: ["React", "Django", "PostgreSQL"],
-    github: "https://github.com/Andreia797"
+    github: "https://github.com/Andreia797",
+    image: "/TelaLoginNhaLixo.PNG"
   },
   {
     id: "3",
     title: "projectItems.3.title",
     description: "projectItems.3.description",
     tech: ["Spring Boot", "React", "ONNX", "Docker"],
-    github: "https://github.com/SentimentONE"
+    github: "https://github.com/SentimentONE",
+    image: "/TelaLoginSentimentOne.PNG"
   }
 ];
 
